4970afb893f5302528e2b208a210052be5cf2359
moustafabazzy <bazzy.moustafa@outlook.com>
2020-09-25 17:25:27 +0300

    MOD login

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+// Services
+import { UserApiService } from '../services/api';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserApiService,
+    private router: Router
+  ) {
+    this.initLoginForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  /**
+   *
+   */
+  initLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  /**
+   *
+   */
+  onSubmit() {
+    let credentials = this.loginForm.value;
+    let loginResult = this.userService.login(credentials.email, credentials.password);
+
+    if (loginResult) {
+      this.router.navigate(['/home']);
+    }
   }
 
 }
