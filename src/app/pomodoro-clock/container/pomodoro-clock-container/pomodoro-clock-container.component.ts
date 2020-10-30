b97a0d6d9fb50d3e46485bdbb7d7ca2abb57fc41
moustafabazzy <bazzy.moustafa@outlook.com>
2020-10-30 13:06:02 +0200

    - MOD pomodoro clock

--- src/app/pomodoro-clock/container/pomodoro-clock-container/pomodoro-clock-container.component.ts
+++ src/app/pomodoro-clock/container/pomodoro-clock-container/pomodoro-clock-container.component.ts
@@ -1,21 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'pomodoro-clock-container',
   templateUrl: './pomodoro-clock-container.component.html',
-  styleUrls: ['./pomodoro-clock-container.component.scss']
+  styleUrls: ['./pomodoro-clock-container.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PomodoroClockContainerComponent implements OnInit {
 
   constructor() { }
 
-  config = { leftTime: 5000 };
+  config = {
+    leftTime: 1500,
+    demand: true,
+    format: 'mm:ss',
+    prettyText: this.prettyText
+  };
+  state: string;
 
   ngOnInit(): void {
   }
 
   handleEvent(event) {
-    console.log(event);
+    this.state = event.action;
   }
 
+  prettyText(text: string): string {
+    return '<div class="text-time">' + text + '</div>';
+  }
+
+  start(countdown): void {
+    if (this.state === 'pause') {
+      countdown.resume();
+      return;
+    }
+
+    countdown.begin();
+  }
+
+  pause(countdown): void {
+    countdown.pause();
+  }
+
+  reset(countdown): void {
+    countdown.restart();
+    countdown.begin();
+  }
 }
